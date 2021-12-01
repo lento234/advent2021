04ad3f1b4275f1539ef7424f6b662f872ae6b4c5
Lento Manickathan <lentou@elbaf.manickathan.ch>
2021-12-01 21:36:02 +0100

    Refactored. better loader

--- day01/src/main.cpp
+++ day01/src/main.cpp
@@ -11,14 +11,14 @@
 static uint32_t problem1(std::string filename)
 {
     // Read file
-    auto text = Message<uint32_t>(filename);
+    auto text = Text<uint32_t>(filename);
 
     // Number of valid depths
     uint32_t answer = 0;
 
     // Check number of increasing depths
     uint32_t prev_depth = -1;
-    for (auto depth : text.content)
+    for (auto depth : text)
     {
         // Check if new depth is higher, then is valid
         if (depth > prev_depth)
@@ -32,7 +32,7 @@ static uint32_t problem1(std::string filename)
 static uint32_t problem2(std::string filename)
 {
     // Read file
-    auto text = Message<uint32_t>(filename);
+    auto text = Text<uint32_t>(filename);
     
     // Answer
     uint32_t answer = 0;
@@ -41,7 +41,7 @@ static uint32_t problem2(std::string filename)
     uint32_t prev_sum = -1, new_sum;
     for (size_t i=1; i<text.size()-1; ++i)
     {
-        new_sum = text.content[i-1] + text.content[i] + text.content[i+1];
+        new_sum = text[i-1] + text[i] + text[i+1];
         if (new_sum > prev_sum)
             answer++;
         prev_sum = new_sum;
@@ -52,32 +52,34 @@ static uint32_t problem2(std::string filename)
 
 int main()
 {
+    auto start = std::chrono::high_resolution_clock::now();
+
+    constexpr uint8_t day = 1;
+
     // Header info
-    constexpr int day = 1;
     fmt::print("\n🎄 Advent of Code: Day {} 🎄\n", day);
     fmt::print("---------------------------\n\n");
-    
+
     // Test input
     uint32_t test_answer1 = problem1("test_input.txt");
-    fmt::print(">> [Test] Problem 1: answer = {} [{}]\n", test_answer1, 
-                passed_or_failed(test_answer1, 7));
+    fmt::print(">> [Test] Problem 1: answer = {} [{}]\n", 
+        test_answer1, passed_or_failed(test_answer1, 7));
+    
     uint32_t test_answer2 = problem2("test_input.txt");
-    fmt::print(">> [Test] Problem 2: answer = {} [{}]\n\n", test_answer2, 
-                passed_or_failed(test_answer2, 5));
+    fmt::print(">> [Test] Problem 2: answer = {} [{}]\n\n", 
+        test_answer2, passed_or_failed(test_answer2, 5));
     
     // Problem 1
-    auto start = std::chrono::high_resolution_clock::now();
     uint32_t answer1 = problem1("input.txt");
-    auto end = std::chrono::high_resolution_clock::now();
-    fmt::print(">> Problem 1: answer = {} [{} μs]\n", answer1, 
-                std::chrono::duration_cast<std::chrono::microseconds>(end - start).count());
-    
+    fmt::print(">> Problem 1: answer = {}\n", answer1);
 
     // Problem 2
-    start = std::chrono::high_resolution_clock::now();
     uint32_t answer2 = problem2("input.txt");
-    end = std::chrono::high_resolution_clock::now();
-    fmt::print(">> Problem 2: answer = {} [{} μs]\n", answer2, 
-                std::chrono::duration_cast<std::chrono::microseconds>(end - start).count());
+    fmt::print(">> Problem 2: answer = {}\n", answer2);
+    
+    // Summary
+    auto end = std::chrono::high_resolution_clock::now();
+    fmt::print("\n>> [Summary] Total elapsed = {} μs\n\n",
+        std::chrono::duration_cast<std::chrono::microseconds>(end - start).count());
     
 }
--- lib/util.h
+++ lib/util.h
@@ -3,6 +3,7 @@
 #include <string>
 #include <vector>
 #include <fstream>
+#include <fmt/ranges.h>
 
 inline std::string passed_or_failed(uint32_t answer, uint32_t truth)
 {
@@ -10,29 +11,35 @@ inline std::string passed_or_failed(uint32_t answer, uint32_t truth)
 }
 
 template <typename T>
-struct Message
+struct Text
 {
-    std::vector<T> content;
-    size_t n_lines;
+    std::vector<T> raw;
     
     //inline std::vector<std::string> read_file(const std::string& filename)
-    Message(const std::string& filename)
+    Text(const std::string& filename)
     {
         // Read file
         std::ifstream file(filename);
-        if (!file.good())
-            throw std::runtime_error("Failed to open!");
+        if (!file.is_open())
+            throw std::runtime_error("Failed to open file!");
 
         // Buffer file
         T buffer;
         while (file >> buffer)
-            content.push_back(buffer);
-        
-        // Close the file.
-        file.close();
-       
+            raw.push_back(buffer);
+        file.close();       
     }
 
-    inline size_t size() { return content.size(); }
+    inline size_t size() { return raw.size(); }
+
+    void print()
+    {
+        fmt::print("  {}\n\n", fmt::join(raw, "\n  "));
+    }
+
+    T& operator[](const size_t i) { return raw[i]; }
+
+    auto begin() { return raw.begin(); }
+    auto end() { return raw.end(); }
 
 };
