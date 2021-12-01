1b07c16f6e1407549d0eeb080b6b13f685f44dca
MrLento234 <lento.manickathan@gmail.com>
2021-12-01 14:00:27 +0100

    test added

--- day01/src/main.cpp
+++ day01/src/main.cpp
@@ -1,96 +1,43 @@
-// Advent of Code: Day 2
+// Advent of Code: Day 1
 // Lento Manickathan
-
 #include <fstream>
 #include <vector>
 #include <string>
-#include <algorithm>
 #include <fmt/ranges.h>
 
-/*
-static int problem1(std::string filename)
+static uint32_t problem1(std::string filename)
 {
     // Read file
     std::ifstream file(filename);
     if (!file.good())
         throw std::runtime_error("Failed to open!");
 
-    int total_valid = 0;
+    // Number of valid depths
+    uint32_t n = 0;
 
-    std::string range, letter, word;
-    while (file >> range >> letter >> word)
+    // Check number of increasing depths
+    uint32_t old_depth = -1, new_depth;
+    while (file >> new_depth)
     {
-        // Inspect input
-        //fmt::print("Range: {}, Letter: {}, Word: {}\n", range, letter, word);
-
-        // Calculate min/max range
-        int min = std::stoi(range);
-        int max = std::stoi(range.substr(range.find('-')+1));
-        //fmt::print("start: {}, end: {}\n", min, max);
-
-        // Calculate number of items
-        char token = letter[0];
-        int count = 0;
-        for (auto& s: word)
-            if (s == token) count++;
-        //fmt::print("counts: {}\n", count);
-        
-        if ( (count >= min) && (count <= max) )
-            total_valid++;
+        if (new_depth > old_depth)
+            n++;
+        old_depth = new_depth;
     }
-    file.close();
-
-    return total_valid;
+    
+    return n;
 }
-
-static int problem2(std::string filename)
-{
-    // Read file
-    std::ifstream file(filename);
-    if (!file.good())
-        throw std::runtime_error("Failed to open!");
-
-    int total_valid = 0;
-
-    std::string range, letter, word;
-    while (file >> range >> letter >> word)
-    {
-        // Inspect input
-        //fmt::print("Range: {}, Letter: {}, Word: {}\n", range, letter, word);
-
-        // Calculate min/max range
-        int min = std::stoi(range);
-        int max = std::stoi(range.substr(range.find('-')+1));
-        //fmt::print("start: {}, end: {}\n", min, max);
-
-        // Calculate number of items
-        char token = letter[0];
-        bool is_valid = ((word[min-1] == token) ^ (word[max-1] == token)) ? true : false;
-        //fmt::print("is_valid: {}\n", is_valid);
-        
-        if (is_valid)
-            total_valid++;
-    }
-    file.close();
-
-    return total_valid;
-}
-*/
 
 int main()
 {
-    // Info
-    const int day = 1;
-    fmt::print("Advent of Code : Day {}\n", day);
+    // Header info
+    constexpr int day = 1;
+    fmt::print("//////////////////////////////\n");
+    fmt::print("    Advent of Code : Day {}   \n", day);
+    fmt::print("//////////////////////////////\n\n");
 
-    /*
-    int test_answer = problem1("test_input.txt");
-    fmt::print("Test answer 1: {}\n\n", test_answer);
+    // Test input
+    uint32_t test_answer1 = problem1("test_input.txt");
+    fmt::print(">> Test answer 1: {}\n", test_answer1);
+    fmt::print(">> Test 1: {}\n", (test_answer1 == 7) ? "PASSED" : "FAILED");
 
-    int answer1 = problem1("input.txt");
-    fmt::print("Answer 1: {}\n\n", answer1);
-    
-    int answer2 = problem2("input.txt");
-    fmt::print("Answer 2: {}\n\n", answer2);
-    */
 }
