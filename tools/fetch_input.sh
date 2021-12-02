87ddcf2ed5faf71678079a7c06fb89b924c3f7c7
Lento Manickathan <lentou@elbaf.manickathan.ch>
2021-12-02 21:35:07 +0100

    automatically download input from adventofcode.com

---
+++ tools/fetch_input.sh
@@ -0,0 +1,6 @@
+#!/bin/env bash
+
+DAY=$1
+
+# Fetch input data of day $DAY from adventofcode.com using curl and session cookie stored in .cookie file
+curl -s -H "Cookie: session=$(cat .cookie)" https://adventofcode.com/2021/day/${DAY#0}/input > ../day$DAY/input.txt
