fcd6442b9958ed2972e9201cf5af8f934b8e7fb2
Lento Manickathan <lentou@elbaf.manickathan.ch>
2021-12-05 17:52:46 +0100

    added remove HTML tags

--- tools/fetch_readme.sh
+++ tools/fetch_readme.sh
@@ -2,5 +2,11 @@
 
 DAY=$1
 
-# Fetch day $DAY and append to README.md using pandoc
-curl -s -H "Cookie: session=$(cat .cookie)" https://adventofcode.com/2021/day/${DAY#0} | pandoc -f html -t markdown | sed '/<div>/,/::: {role="main"}/d' | sed '/At this point/,//d' >> ../day$DAY/README.md
+# Fetch day $DAY using curl and cookie
+# convert from HTML to pandoc
+# delete all unwanted lines and HTML tags containing {}
+# write to README.md
+curl -s -H "Cookie: session=$(cat .cookie)" https://adventofcode.com/2021/day/${DAY#0} \
+    | pandoc -f html -t markdown \
+    | sed -e '/<div>/,/::: {role="main"}/d' -e '/At this point/,//d' -e 's/{[^{}]*}/ /g' \
+    >> ../day$DAY/README.md
