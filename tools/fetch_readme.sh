707e287e81cd5ef7f16a8f970ba0ee4ad4f6a676
Lento Manickathan <lentou@elbaf.manickathan.ch>
2021-12-05 17:33:52 +0100

    updated function to automatically delete unwanted lines

--- tools/fetch_readme.sh
+++ tools/fetch_readme.sh
@@ -3,4 +3,4 @@
 DAY=$1
 
 # Fetch day $DAY and append to README.md using pandoc
-curl -s -H "Cookie: session=$(cat .cookie)" https://adventofcode.com/2021/day/${DAY#0} | pandoc -f html -t markdown >> ../day$DAY/README.md
+curl -s -H "Cookie: session=$(cat .cookie)" https://adventofcode.com/2021/day/${DAY#0} | pandoc -f html -t markdown | sed '/<div>/,/::: {role="main"}/d' | sed '/At this point/,//d' >> ../day$DAY/README.md
