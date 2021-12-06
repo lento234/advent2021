aa77a7c98b2f30c2ce5c4e926431074a89772afa
Lento Manickathan <lentou@elbaf.manickathan.ch>
2021-12-06 10:57:13 +0100

    added two diff version, str and int

--- include/str.h
+++ include/str.h
@@ -11,13 +11,24 @@ static inline bool contains(const std::string& str, const std::string& match)
     return str.find(match) != std::string::npos;
 }
 
-static inline std::vector<std::string> split(const std::string& str, const char& delim)
+static inline std::vector<std::string> split_string(const std::string& str, const char& delim)
 {
     std::vector<std::string> result;
     std::stringstream ss(str);
 
     for (std::string token; getline(ss, token, delim);)
         result.push_back(token);
+    return result;
+}
+
+template <typename T>
+static inline std::vector<T> split_numbers(const std::string& str, const char& delim)
+{
+    std::vector<T> result;
+    std::stringstream ss(str);
+
+    for (std::string token; getline(ss, token, delim);)
+        result.push_back(std::stoi(token));
     return result;
 }
 
