069031c3c7d176b542dd5222ce85ee909095a189
MrLento234 <lento.manickathan@gmail.com>
2021-12-08 09:12:27 +0100

    will fail if dir already exists

--- tools/new_day.sh
+++ tools/new_day.sh
@@ -2,9 +2,9 @@
 
 DAY=$1
 
+# Make new directory
 # Copy template to new day
-cp -rv ../template/day00 ../day$DAY
-
-# Replace all occurances of 00 to new date
-find ../day$DAY -type f -exec sed -i "s/00/$DAY/g" {} \;
+mkdir ../day$DAY &&\
+    cp -rv ../template/day00/* ../day$DAY/ &&\
+    find ../day$DAY -type f -exec sed -i "s/00/$DAY/g" {} \;
 
