5afa053471f4b4ddde771c82f2d42250c2164509
MrLento234 <lento.manickathan@gmail.com>
2021-12-08 15:07:11 +0100

    solved part 1

--- day08/src/main.cpp
+++ day08/src/main.cpp
@@ -8,15 +8,24 @@
 #include <utils/parser.h>
 #include <utils/timer.h>
 
-static int64_t problem1(utils::Text<std::string>& input)
+static uint64_t problem1(utils::Text<std::string>& input)
 {
     // Answer
-    int64_t answer = 0;
+    uint64_t answer = 0;
 
+    for (size_t i = 0; i < input.size(); i = i + 15)
+    {
+        for (size_t j = 0; j < 4; ++j)
+        {
+            size_t word_size = input[i + 11 + j].size();
+            if (word_size == 2 || word_size == 3 || word_size == 4 || word_size == 7)
+                answer++;
+        }
+    }
     return answer;
 }
 
-static int64_t problem2(utils::Text<std::string>& input)
+static uint64_t problem2(utils::Text<std::string>& input)
 {
     // Answer
     int64_t answer = 0;
@@ -29,7 +38,7 @@ int main()
     auto timeit = utils::Timer();
 
     constexpr uint8_t day = 8;
-    
+
     // Header info
     fmt::print("\n🎄 Advent of Code: Day {} 🎄\n", day);
     fmt::print("---------------------------\n\n");
@@ -37,23 +46,21 @@ int main()
     // Test input
     auto test_input = utils::Text<std::string>("test_input.txt");
 
-    test_input.print();
+    uint64_t test_answer1 = problem1(test_input);
+    fmt::print(">> [Test] Problem 1: answer = {} [{}]\n",
+               test_answer1,
+               utils::pass_or_fail<uint64_t>(test_answer1, 26));
 
-    // int64_t test_answer1 = problem1(test_input);
-    // fmt::print(">> [Test] Problem 1: answer = {} [{}]\n",
-    //            test_answer1,
-    //            utils::pass_or_fail<uint32_t>(test_answer1, 0));
-
-    // int64_t test_answer2 = problem2(test_input);
-    // fmt::print(">> [Test] Problem 2: answer = {} [{}]\n\n",
-    //            test_answer2,
-    //            utils::pass_or_fail<uint32_t>(test_answer2, 0));
+    uint64_t test_answer2 = problem2(test_input);
+    fmt::print(">> [Test] Problem 2: answer = {} [{}]\n\n",
+               test_answer2,
+               utils::pass_or_fail<uint64_t>(test_answer2, 61229));
 
     // // Real input
-    // auto input = utils::Text<std::string>("input.txt");
+    auto input = utils::Text<std::string>("input.txt");
 
-    // // Problem 1
-    // fmt::print(">> Problem 1: answer = {}\n", problem1(input));
+    // Problem 1
+    fmt::print(">> Problem 1: answer = {}\n", problem1(input));
 
     // // Problem 2
     // fmt::print(">> Problem 2: answer = {}\n", problem2(input));
