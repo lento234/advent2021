776717f7572c8dceac6b2c2ba38a85b8481a4c30
MrLento234 <lento.manickathan@gmail.com>
2021-12-08 09:18:25 +0100

    initialized day08

---
+++ day08/src/main.cpp
@@ -0,0 +1,60 @@
+// Advent of Code: Day 08
+// Lento Manickathan
+#include <chrono>
+#include <fmt/ranges.h>
+#include <string>
+#include <vector>
+
+#include <utils/parser.h>
+#include <utils/timer.h>
+
+static int64_t problem1(utils::Text<std::string>& input)
+{
+    // Answer
+    int64_t answer = 0;
+
+    return answer;
+}
+
+static int64_t problem2(utils::Text<std::string>& input)
+{
+    // Answer
+    int64_t answer = 0;
+
+    return answer;
+}
+
+int main()
+{
+    auto timeit = utils::Timer();
+
+    constexpr uint8_t day = 8;
+    
+    // Header info
+    fmt::print("\n🎄 Advent of Code: Day {} 🎄\n", day);
+    fmt::print("---------------------------\n\n");
+
+    // Test input
+    auto test_input = utils::Text<std::string>("test_input.txt");
+
+    test_input.print();
+
+    // int64_t test_answer1 = problem1(test_input);
+    // fmt::print(">> [Test] Problem 1: answer = {} [{}]\n",
+    //            test_answer1,
+    //            utils::pass_or_fail<uint32_t>(test_answer1, 0));
+
+    // int64_t test_answer2 = problem2(test_input);
+    // fmt::print(">> [Test] Problem 2: answer = {} [{}]\n\n",
+    //            test_answer2,
+    //            utils::pass_or_fail<uint32_t>(test_answer2, 0));
+
+    // // Real input
+    // auto input = utils::Text<std::string>("input.txt");
+
+    // // Problem 1
+    // fmt::print(">> Problem 1: answer = {}\n", problem1(input));
+
+    // // Problem 2
+    // fmt::print(">> Problem 2: answer = {}\n", problem2(input));
+}
