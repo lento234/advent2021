0b79490851471e4efcba3b776d193fd3ffca634f
Manickathan, Lento <lento.manickathan@empa.ch>
2021-12-10 15:52:46 +0100

    removed unnecessary operations. most definetly compiler removes it anyway

--- day09/src/core.h
+++ day09/src/core.h
@@ -6,10 +6,10 @@
 static inline bool stencil_min(const std::vector<std::string>& height, const size_t& i, const size_t& j)
 {
     // clang-format off
-    return (((height[i][j] - '0') < (height[i    ][j + 1] - '0')) 
-         && ((height[i][j] - '0') < (height[i    ][j - 1] - '0'))
-         && ((height[i][j] - '0') < (height[i + 1][j    ] - '0'))
-         && ((height[i][j] - '0') < (height[i - 1][j    ] - '0')));
+    return ((height[i][j] < height[i    ][j + 1]) 
+         && (height[i][j] < height[i    ][j - 1])
+         && (height[i][j] < height[i + 1][j    ])
+         && (height[i][j] < height[i - 1][j    ]));
     // clang-format on
 }
 
@@ -18,7 +18,7 @@ static inline uint16_t spread(const std::vector<std::string>& height,
                               const size_t& i,
                               const size_t& j)
 {
-    if ((height[i][j] - '0') == 9 || (visited[i][j] - '0') == 1)
+    if ((height[i][j] == '9') || (visited[i][j] == '1'))
         return 0;
     else
     {
