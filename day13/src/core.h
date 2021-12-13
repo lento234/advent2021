38bed5a54540c785f25bda695a13aaa42ebde48b
MrLento234 <lento.manickathan@gmail.com>
2021-12-13 17:06:17 +0100

    cleanup format

---
+++ day13/src/core.h
@@ -0,0 +1,81 @@
+#pragma once
+
+#include <string>
+#include <vector>
+
+#include <utils/parser.h>
+
+struct Origami
+{
+    size_t x_max = 0, y_max = 0;
+    size_t size = 0, n_dots = 0;
+
+    std::vector<uint16_t> x_coordinates;
+    std::vector<uint16_t> y_coordinates;
+    std::vector<std::string> grid;
+
+    Origami() = default;
+
+    Origami(utils::Text<std::string>& input)
+    {
+        for (auto& line : input)
+        {
+            if (line[0] != 'f' && line[0] != 'a' && line[0] != 'y' && line[0] != 'x')
+            {
+                std::vector<uint16_t> xy_pair = utils::split_numbers<uint16_t>(line, ',');
+                x_coordinates.push_back(xy_pair[0]);
+                y_coordinates.push_back(xy_pair[1]);
+            }
+        }
+        x_max = *std::max_element(x_coordinates.begin(), x_coordinates.end());
+        y_max = *std::max_element(y_coordinates.begin(), y_coordinates.end());
+        size = x_coordinates.size();
+    }
+
+    void fold_up(uint16_t y)
+    {
+        for (size_t i = 0; i < size; i++)
+        {
+            if (y_coordinates[i] > y)
+                y_coordinates[i] = 2 * y - y_coordinates[i];
+        }
+        y_max = y - 1;
+    }
+
+    void fold_left(uint16_t x)
+    {
+        for (size_t i = 0; i < size; i++)
+        {
+            if (x_coordinates[i] > x)
+                x_coordinates[i] = 2 * x - x_coordinates[i];
+        }
+        x_max = x - 1;
+    }
+
+    void make_grid()
+    {
+        grid = std::vector<std::string>(y_max + 1, std::string(x_max + 1, '.'));
+        for (size_t i = 0; i < size; ++i)
+            grid[y_coordinates[i]][x_coordinates[i]] = '#';
+
+        n_dots = 0;
+        for (auto& line : grid)
+            n_dots += std::count(line.begin(), line.end(), '#');
+    }
+
+    void print(const std::string& style = "grid")
+    {
+        if (style == "raw")
+        {
+            for (size_t i = 0; i < size; ++i)
+                fmt::print("{} {}\n", x_coordinates[i], y_coordinates[i]);
+            fmt::print("Origami: size = {}, x_max = {} y_max = {}\n\n", size, x_max, y_max);
+        }
+        else
+        {
+            make_grid();
+            fmt::print("\n    {}\n\n", fmt::join(grid, "\n    "));
+            //fmt::print("Origami: size = {}, x_max = {}, y_max = {}, number of # = {}\n\n", size, x_max, y_max, n_dots);
+        }
+    }
+};
--- day13/src/main.cpp
+++ day13/src/main.cpp
@@ -9,80 +9,7 @@
 #include <utils/parser.h>
 #include <utils/timer.h>
 
-struct Origami
-{
-    size_t x_max = 0, y_max = 0;
-    size_t size = 0, n_dots = 0;
-
-    std::vector<uint16_t> x_coordinates;
-    std::vector<uint16_t> y_coordinates;
-    std::vector<std::string> grid;
-
-    Origami() = default;
-
-    Origami(utils::Text<std::string>& input)
-    {
-        for (auto& line : input)
-        {
-            if (line[0] != 'f' && line[0] != 'a' && line[0] != 'y' && line[0] != 'x')
-            {
-                std::vector<uint16_t> xy_pair = utils::split_numbers<uint16_t>(line, ',');
-                x_coordinates.push_back(xy_pair[0]);
-                y_coordinates.push_back(xy_pair[1]);
-            }
-        }
-        x_max = *std::max_element(x_coordinates.begin(), x_coordinates.end());
-        y_max = *std::max_element(y_coordinates.begin(), y_coordinates.end());
-        size = x_coordinates.size();
-    }
-
-    void fold_up(uint16_t y)
-    {
-        for (size_t i = 0; i < size; i++)
-        {
-            if (y_coordinates[i] > y)
-                y_coordinates[i] = 2 * y - y_coordinates[i];
-        }
-        y_max = y-1;
-    }
-
-    void fold_left(uint16_t x)
-    {
-        for (size_t i = 0; i < size; i++)
-        {
-            if (x_coordinates[i] > x)
-                x_coordinates[i] = 2 * x - x_coordinates[i];
-        }
-        x_max = x-1;
-    }
-
-    void make_grid()
-    {
-        grid = std::vector<std::string>(y_max + 1, std::string(x_max + 1, '.'));
-        for (size_t i = 0; i < size; ++i)
-            grid[y_coordinates[i]][x_coordinates[i]] = '#';
-
-        n_dots = 0;
-        for (auto& line : grid)
-            n_dots += std::count(line.begin(), line.end(), '#');
-    }
-
-    void print(const std::string& style = "grid")
-    {
-        if (style == "raw")
-        {
-            for (size_t i = 0; i < size; ++i)
-                fmt::print("{} {}\n", x_coordinates[i], y_coordinates[i]);
-            fmt::print("Origami: size = {}, x_max = {} y_max = {}\n\n", size, x_max, y_max);
-        }
-        else
-        {
-            make_grid();
-            fmt::print("{}\n", fmt::join(grid, "\n"));
-            fmt::print("Origami: size = {}, x_max = {}, y_max = {}, number of # = {}\n\n", size, x_max, y_max, n_dots);
-        }
-    }
-};
+#include "core.h"
 
 static int64_t problem1(utils::Text<std::string>& input)
 {
@@ -90,7 +17,7 @@ static int64_t problem1(utils::Text<std::string>& input)
 
     size_t initial_size = origami.size;
 
-    for (size_t i = initial_size; i < initial_size+3; ++i)
+    for (size_t i = initial_size; i < initial_size + 3; ++i)
     {
         auto& line = input[i];
         if (line[0] == 'y')
@@ -107,7 +34,7 @@ static int64_t problem1(utils::Text<std::string>& input)
     return answer;
 }
 
-static std::string problem2(utils::Text<std::string>& input)
+static void problem2(utils::Text<std::string>& input)
 {
     Origami origami(input);
 
@@ -122,11 +49,6 @@ static std::string problem2(utils::Text<std::string>& input)
             origami.fold_left(std::stoi(line.substr(2)));
     }
     origami.print(); // origami.make_grid() and print;
-    
-    // Answer
-    int64_t answer = origami.n_dots;
-
-    return answer;
 }
 
 int main()
@@ -147,10 +69,8 @@ int main()
                test_answer1,
                utils::pass_or_fail<uint32_t>(test_answer1, 17));
 
-    // int64_t test_answer2 = problem2(test_input);
-    // fmt::print(">> [Test] Problem 2: answer = {} [{}]\n\n",
-    //            test_answer2,
-    //            utils::pass_or_fail<uint32_t>(test_answer2, 0));
+    fmt::print(">> [Test] Problem 2: answer = \n");
+    problem2(test_input);
 
     // Real input
     auto input = utils::Text<std::string>("input.txt");
@@ -158,6 +78,7 @@ int main()
     // Problem 1
     fmt::print(">> Problem 1: answer = {}\n", problem1(input));
 
-    // // Problem 2
-    // fmt::print(">> Problem 2: answer = {}\n", problem2(input));
+    // Problem 2
+    fmt::print(">> Problem 2: answer = \n");
+    problem2(input);
 }
