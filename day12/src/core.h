b8d1a8501df338daac6f91730c8b7512ba2fb907
MrLento234 <lento.manickathan@gmail.com>
2021-12-13 13:56:49 +0100

    was challenging

--- day12/src/core.h
+++ day12/src/core.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <algorithm>
 #include <map>
 #include <memory>
 #include <string>
@@ -14,6 +15,7 @@ struct Node
 
     value_t name;
     bool is_big_cave = false;
+    size_t n_neighbours = 0;
 
     std::map<value_t, std::shared_ptr<Node>> neighbours;
     static std::vector<nodelist_t> routes;
@@ -29,7 +31,10 @@ struct Node
     void add_neighbour(std::shared_ptr<Node> new_neighbour)
     {
         if (name != "end" && ((neighbours.find(new_neighbour->name) == neighbours.end()) && new_neighbour->name != "start"))
+        {
             neighbours[new_neighbour->name] = new_neighbour;
+            n_neighbours++;
+        }
     }
 
     nodelist_t get_neighbour_names()
@@ -40,33 +45,47 @@ struct Node
         return neighbour_name_list;
     }
 
-    void find_all_paths_to(const value_t& destination)
+    void find_all_paths_to(const value_t& destination, int8_t n_visits = 1)
     {
+        routes.clear();
         nodelist_t path = {name};
 
         for (auto& [next_node_name, next_node] : neighbours)
         {
             nodelist_t new_path = path;
             new_path.push_back(next_node_name);
-            next_node->travel(new_path, destination);
+            next_node->travel(new_path, destination, n_visits);
         }
     }
 
-    void travel(nodelist_t path, const value_t& destination)
+    void travel(nodelist_t path, const value_t& destination, int8_t n_visits)
     {
         for (auto& [next_node_name, next_node] : this->neighbours)
         {
             if (next_node_name == destination)
             {
+                // end of the journey
                 nodelist_t final_path(path);
                 final_path.push_back(next_node_name);
                 routes.push_back(final_path);
             }
-            else if (!utils::is_inside<std::string>(path, next_node_name) || next_node->is_big_cave)
+            else if (next_node->is_big_cave)
             {
+                // Someone else always has to carry on the story
                 nodelist_t new_path = path;
                 new_path.push_back(next_node_name);
-                next_node->travel(new_path, destination);
+                next_node->travel(new_path, destination, n_visits);
+            }
+            else if (next_node_name != "start" && std::count(path.begin(), path.end(), next_node_name) < n_visits) // number of times we can visit a small case
+            {
+                // We can visit this cave
+                nodelist_t new_path = path;
+                new_path.push_back(next_node_name);
+
+                if (std::count(path.begin(), path.end(), next_node_name))
+                    next_node->travel(new_path, destination, n_visits-1);
+                else
+                    next_node->travel(new_path, destination, n_visits);          
             }
         }
     }
--- day12/src/main.cpp
+++ day12/src/main.cpp
@@ -11,85 +11,6 @@
 
 #include "core.h"
 
-// struct Node
-// {
-//     using value_t = std::string;
-//     using nodelist_t = std::vector<value_t>;
-
-//     value_t name;
-//     bool is_big_cave = false;
-
-//     nodelist_t neighbours;
-
-//     Node() = default;
-
-//     Node(value_t name) : name(name)
-//     {
-//         if (std::isupper(name[0]))
-//             is_big_cave = true;
-//     }
-
-//     void add_neighbour(value_t neighbour_name)
-//     {
-//         if (name != "end" && (!utils::is_inside<value_t>(neighbours, neighbour_name) && neighbour_name != "start"))
-//             neighbours.push_back(neighbour_name);
-//     }
-// };
-
-// void travel(
-//   std::map<std::string, Node>& graph,
-//   std::vector<Node::nodelist_t>& routes,
-//   Node::nodelist_t path,
-//   const std::string& destination)
-// {
-//     std::string& location = path.back();
-
-//     for (auto next_location : graph[location].neighbours)
-//     {
-//         if (next_location == destination)
-//         {
-//             std::vector<std::string> final_path = path;
-//             final_path.push_back(next_location);
-//             routes.push_back(final_path);
-//         }
-//         else if (!utils::is_inside<std::string>(path, next_location) || graph[next_location].is_big_cave)
-//         {
-//             std::vector<std::string> new_path = path;
-//             new_path.push_back(next_location);
-//             travel(graph, routes, new_path, destination);
-//         }
-//     }
-// }
-
-// static uint64_t problem1(utils::Text<std::string>& input)
-// {
-//     // Store all nodes and paths as a graph
-//     std::map<std::string, Node> graph;
-//     for (auto& line : input)
-//     {
-//         // Split two nodes from the line string
-//         std::vector<std::string> node_pairs = utils::split_string(line, '-');
-
-//         // Make nodes
-//         for (auto& node : node_pairs)
-//             if (graph.find(node) == graph.end())
-//                 graph[node] = Node(node);
-
-//         // Add neighbours to each other
-//         graph[node_pairs[0]].add_neighbour(node_pairs[1]); // node1 -> node2
-//         graph[node_pairs[1]].add_neighbour(node_pairs[0]); // node2 -> node1
-//     }
-
-//     // Find all paths from start to end
-//     std::vector<Node::nodelist_t> routes;
-//     std::vector<Node::value_t> path = {"start"};
-//     travel(graph, routes, path, "end");
-
-//     // Answer
-//     int64_t answer = routes.size();
-
-//     return answer;
-// }
 
 static uint64_t problem1(utils::Text<std::string>& input)
 {
@@ -111,7 +32,7 @@ static uint64_t problem1(utils::Text<std::string>& input)
     }
 
     // Find all paths from start to end
-    graph["start"]->find_all_paths_to("end");
+    graph["start"]->find_all_paths_to("end", 1);
 
     // Answer
     uint64_t answer = graph["start"]->routes.size();
@@ -119,7 +40,7 @@ static uint64_t problem1(utils::Text<std::string>& input)
     return answer;
 }
 
-static int64_t problem2(utils::Text<std::string>& input)
+static uint64_t problem2(utils::Text<std::string>& input)
 {
     // Store all nodes and paths as a graph
     std::map<std::string, std::shared_ptr<Node>> graph;
@@ -139,7 +60,7 @@ static int64_t problem2(utils::Text<std::string>& input)
     }
 
     // Find all paths from start to end
-    graph["start"]->find_all_paths_to("end");
+    graph["start"]->find_all_paths_to("end", 2);
 
     // Answer
     uint64_t answer = graph["start"]->routes.size();
@@ -176,6 +97,6 @@ int main()
     // Problem 1
     fmt::print(">> Problem 1: answer = {}\n", problem1(input));
 
-    // // Problem 2
-    // fmt::print(">> Problem 2: answer = {}\n", problem2(input));
+    // Problem 2
+    fmt::print(">> Problem 2: answer = {}\n", problem2(input));
 }
